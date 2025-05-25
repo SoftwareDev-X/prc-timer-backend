2bc7f8ff419693fd801c7ee0a71605e4b75ff384
Chrisadd23 <haas.christophermichael@gmx.de>
2025-05-25 19:45:25 +0200

    feat: create request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRandomNumber(): Promise<number[]> {
+    return this.appService.getRandomNumber();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+  async getRandomNumber(): Promise<number[]> {
+    const response = await firstValueFrom(
+      this.httpService.get<number[]>(
+        'https://www.randomnumberapi.com/api/v1.0/random',
+      ),
+    );
+    console.log(response.data);
+    return response.data;
   }
 }
